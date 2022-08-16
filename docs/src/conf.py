3a399cad20f5b236eaf2a7683fd5cd201e0eb8a0
Benjamin Mickler <ben@benmickler.com>
2022-08-16 18:24:15 +1000

    documenting

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import sys
-sys.path.append("../")
+sys.path.append("../../")
 
 project = 'pytts'
 copyright = '2022, Benjamin Mickler'
